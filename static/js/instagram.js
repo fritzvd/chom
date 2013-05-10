fb0f92083aaacd1272f23da2256f3f26ea408129
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-05-10 16:44:27 +0200

    new images

--- static/js/instagram.js
+++ static/js/instagram.js
@@ -1,29 +1,3 @@
-    // angular.module('instagram', ['ngResource']).
-    //   factory('Project', function($resource) {
-    //     var Project = $resource('https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4&callback=JSON_CALLBACK',
-    //       {}, {
-    //         get: {method:'GET'},
-    //         replies: {method:'JSONP'}
-    //       });     
-    //   return Project;
-    // });
-
-    // angular.module('project', ['instagram']).
-    //   config(function($routeProvider) {
-    //   $routeProvider.
-    //   when('/', {controller:ListCtrl, templateUrl:'/static/templates/list.html'}).
-    //   otherwise({redirectTo:'/'});
-    // });
-     
-     
-    // function ListCtrl($scope, Project) {
-    //   debugger
-    //   $scope.project = Project.get();
-    // }
-
-    //   // $(window).load(function(){
-    //   //       $('#launch').modal('show');
-    //   //   });
 
     $.ajax({
       url: "https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4", 
