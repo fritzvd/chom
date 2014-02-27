10bd436fdc4bf1a40618efbc7899a5bc839599f2
fritzvd <f.vandeventer@gmail.com>
2014-02-27 16:34:35 +0100

    CORS fuck off

--- static/js/apis.js
+++ static/js/apis.js
@@ -19,10 +19,37 @@ $.ajax({
 
 
 
-$.ajax({
-  url: "http://api.songkick.com/api/3.0/artists/6838049/calendar.json?apikey=sSxFP4FVJ3wcxiBD", 
-  dataType: 'jsonp', 
-  method: 'GET'}).done(function(response){
+// $.getJSON({
+//   url: "http://api.songkick.com/api/3.0/artists/6838049/calendar.json?apikey=sSxFP4FVJ3wcxiBD&jsoncallback=?", 
+//   method: 'GET'}).done(function(response){
+//       console.log(Object.keys(response.resultsPage.results).length)
+//       if (Object.keys(response.resultsPage.results).length < 1) {
+//         $('#comingup').append("<li>We've got nothing lined up in the coming month</li>");
+//       }
+//     $.each(response.resultsPage.results, function(count){
+//       if (count < 8){
+//       $('#comingup').append('<li><a href="'+ this.uri +'">' + this.displayName + '</a></li>')
+//     }
+//     })
+//   });
+
+// $.getJSON({
+//   url: "http://api.songkick.com/api/3.0/artists/6838049/gigography.json?apikey=sSxFP4FVJ3wcxiBD&order=desc&jsoncallback=?", 
+//   method: 'GET'}).done(function(response){
+//     // var response = response();
+//     $.each(response.resultsPage.results.event, function(count){
+//       if (count < 8){
+//         var location = this.venue.displayName;
+//         if (location == 'Unknown venue') {
+//           location = this.venue.metroArea.displayName;
+//         }
+//       $('#past').append('<li><a href="'+ this.uri +'">' + location + ' - ' + this.start.date + '</a></li>')
+//     }
+//     })
+//   });
+
+$.getJSON("http://api.songkick.com/api/3.0/artists/6838049/calendar.json?apikey=sSxFP4FVJ3wcxiBD&order=desc&jsoncallback=?",
+    function(response){
       console.log(Object.keys(response.resultsPage.results).length)
       if (Object.keys(response.resultsPage.results).length < 1) {
         $('#comingup').append("<li>We've got nothing lined up in the coming month</li>");
@@ -34,10 +61,9 @@ $.ajax({
     })
   });
 
-$.ajax({
-  url: "http://api.songkick.com/api/3.0/artists/6838049/gigography.json?apikey=sSxFP4FVJ3wcxiBD&order=desc", 
-  dataType: 'jsonp', 
-  method: 'GET'}).done(function(response){
+$.getJSON("http://api.songkick.com/api/3.0/artists/6838049/gigography.json?apikey=sSxFP4FVJ3wcxiBD&order=desc&jsoncallback=?",
+  function(response){
+    // var response = response();
     $.each(response.resultsPage.results.event, function(count){
       if (count < 8){
         var location = this.venue.displayName;
