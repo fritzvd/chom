ef48a573697b5383a13488b22f2f732f64565e52
fritzvd <f.vandeventer@gmail.com>
2014-02-28 11:17:51 +0100

    events idiot

--- static/js/apis.js
+++ static/js/apis.js
@@ -1,16 +1,16 @@
-
 $.ajax({
-  url: "https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4", 
-  dataType: 'jsonp', 
-  method: 'GET'}).done(function(response){
-    $.each(response.data, function(count){
-    	if (count < 8){
-      $('#images').append('<a href="/news/"><img class="instagram" src="'+ this.images.thumbnail.url +'" /></a>')
-    }
+    url: "https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4",
+    dataType: 'jsonp',
+    method: 'GET'
+}).done(function(response) {
+    $.each(response.data, function(count) {
+        if (count < 8) {
+            $('#images').append('<a href="/news/"><img class="instagram" src="' + this.images.thumbnail.url + '" /></a>')
+        }
     })
-  })
+})
 
-  
+
 // past
 //   http://api.songkick.com/api/3.0/artists/{artist_id}/gigography.json?apikey=sSxFP4FVJ3wcxiBD
 
@@ -49,28 +49,32 @@ $.ajax({
 //   });
 
 $.getJSON("http://api.songkick.com/api/3.0/artists/6838049/calendar.json?apikey=sSxFP4FVJ3wcxiBD&order=desc&jsoncallback=?",
-    function(response){
-      console.log(Object.keys(response.resultsPage.results).length)
-      if (Object.keys(response.resultsPage.results).length < 1) {
-        $('#comingup').append("<li>We've got nothing lined up in the coming month</li>");
-      }
-    $.each(response.resultsPage.results, function(count){
-      if (count < 8){
-      $('#comingup').append('<li><a href="'+ this.uri +'">' + this.displayName + '</a></li>')
-    }
-    })
-  });
+    function(response) {
+        console.log(Object.keys(response.resultsPage.results).length)
+        if (Object.keys(response.resultsPage.results).length < 1) {
+            $('#comingup').append("<li>We've got nothing lined up in the coming month</li>");
+        }
+        $.each(response.resultsPage.results.event, function(count) {
+            if (count < 8) {
+                var location = this.venue.displayName;
+                if (location == 'Unknown venue') {
+                    location = this.venue.metroArea.displayName;
+                }
+                $('#comingup').append('<li><a href="' + this.uri + '">' + location + ' - ' + this.start.date + '</a></li>')
+            }
+        })
+    });
 
 $.getJSON("http://api.songkick.com/api/3.0/artists/6838049/gigography.json?apikey=sSxFP4FVJ3wcxiBD&order=desc&jsoncallback=?",
-  function(response){
-    // var response = response();
-    $.each(response.resultsPage.results.event, function(count){
-      if (count < 8){
-        var location = this.venue.displayName;
-        if (location == 'Unknown venue') {
-          location = this.venue.metroArea.displayName;
-        }
-      $('#past').append('<li><a href="'+ this.uri +'">' + location + ' - ' + this.start.date + '</a></li>')
-    }
-    })
-  });
+    function(response) {
+        // var response = response();
+        $.each(response.resultsPage.results.event, function(count) {
+            if (count < 8) {
+                var location = this.venue.displayName;
+                if (location == 'Unknown venue') {
+                    location = this.venue.metroArea.displayName;
+                }
+                $('#past').append('<li><a href="' + this.uri + '">' + location + ' - ' + this.start.date + '</a></li>')
+            }
+        })
+    });
