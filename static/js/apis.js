e47ce366bc7da328aec8fdf63060e354e25ad79b
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-02-22 15:14:48 +0100

    release front

--- static/js/apis.js
+++ static/js/apis.js
@@ -1,14 +1,14 @@
-$.ajax({
-    url: "https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4",
-    dataType: 'jsonp',
-    method: 'GET'
-}).done(function(response) {
-    $.each(response.data, function(count) {
-        if (count < 8) {
-            $('#images').append('<a href="/news/"><img class="instagram" src="' + this.images.thumbnail.url + '" /></a>')
-        }
-    })
-})
+/*$.ajax({*/
+    //url: "https://api.instagram.com/v1/tags/cheesesofmexico/media/recent?client_id=e8020c94851e4938ae02ca397c441fd4",
+    //dataType: 'jsonp',
+    //method: 'GET'
+//}).done(function(response) {
+    //$.each(response.data, function(count) {
+        //if (count < 8) {
+            //$('#images').append('<a href="/news/"><img class="instagram" src="' + this.images.thumbnail.url + '" /></a>')
+        //}
+    //})
+//})
 
 
 // past
