82e4ff8373b6d607fb872710c1f924e240170bb9
Fritz van Deventer <fritzvd@users.noreply.github.com>
2022-10-31 07:05:37 +0100

    always stays the same

---
+++ boringstranger/gen_page.py
@@ -0,0 +1,25 @@
+from jinja2 import Environment, FileSystemLoader
+import os, sys
+ 
+
+ 
+
+def main():
+    args = sys.argv[1:]
+
+    filename = args[0]
+
+    root = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(root, 'templates')
+    env = Environment( loader = FileSystemLoader(templates_dir) )
+    template = env.get_template('{}.html'.format(filename))
+    
+    
+    filename = os.path.join(root, 'html', 'index.html')
+    with open(filename, 'w') as fh:
+        fh.write(template.render(
+            h1 = "Hello Jinja2",
+            show_one = True,
+            show_two = False,
+            names    = ["Foo", "Bar", "Qux"],
+        ))
