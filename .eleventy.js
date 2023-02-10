33fe2d2a3974628535dcbc5c8c0831580a9e1be9
Fritz van Deventer <fritzvd@users.noreply.github.com>
2023-02-10 14:00:09 +0100

    sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const format = require('date-fns/format')
+
 module.exports = function(eleventyConfig) {
     // Output directory: _site
   
     // Copy  static to _site/static
     eleventyConfig.addPassthroughCopy("static");
+
+    // add `date` filter
+    eleventyConfig.addFilter('date', function (date, dateFormat) {
+      return format(date, dateFormat)
+    })
 
   };
